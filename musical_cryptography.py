af8bb1d01351e339b508022d56a864aee422f6a3
Sarhang Said <59802612+Feynman10@users.noreply.github.com>
2023-08-11 22:53:20 +0200

    musical_cryptography2.py
    Upgraded

--- musical_cryptography.py
+++ musical_cryptography.py
@@ -9,12 +9,37 @@ def text_to_notes(text):
 
 def create_midi(notes, filename):
     mid = MidiFile()
-    track = MidiTrack()
-    mid.tracks.append(track)
     
+    # Melody track
+    track_melody = MidiTrack()
+    mid.tracks.append(track_melody)
+    
+    # Chords track
+    track_chords = MidiTrack()
+    mid.tracks.append(track_chords)
+
+    # Major scale pattern
+    major_scale = [0, 2, 4, 5, 7, 9, 11, 12]
+    
+    # Previous note used for chord generation
+    prev_note = 60
+
     for note in notes:
-        track.append(Message('note_on', note=note, velocity=64, time=32))
-        track.append(Message('note_off', note=note, velocity=64, time=32))
+        # Melody
+        track_melody.append(Message('note_on', note=note, velocity=64, time=32))
+        track_melody.append(Message('note_off', note=note, velocity=64, time=64))
+        
+        # Chords (triads based on major scale)
+        chord_root = note - (note - prev_note) % 12
+        chord_notes = [chord_root, chord_root + major_scale[2], chord_root + major_scale[4]]
+        
+        for chord_note in chord_notes:
+            track_chords.append(Message('note_on', note=chord_note, velocity=50, time=0))
+        
+        for chord_note in chord_notes:
+            track_chords.append(Message('note_off', note=chord_note, velocity=50, time=64))
+        
+        prev_note = note
 
     mid.save(filename)
 
